9ac3d8100240e6cd928218f881026bb9e11e3ad1
Léo Lam <leo@leolam.fr>
2024-07-07 19:28:34 +0100

    Update Versioneer to fix Python 3.12 compat

--- aamp/__init__.py
+++ aamp/__init__.py
@@ -1,6 +1,5 @@
 from aamp.aamp import *
 from aamp.parameters import *
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
